68ba0a358c5ed5bff084df10b4fefd7249185591
Matthew Cain <matthew.cain@tuta.io>
2022-02-07 14:09:25 +0100

    Create JsonObjectBuilder class

---
+++ src/main/java/io/matshou/cata/tilecov/json/JsonObjectBuilder.java
@@ -0,0 +1,123 @@
+package io.matshou.cata.tilecov.json;
+
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.Nullable;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.reflect.TypeToken;
+
+/**
+ * This class can build a class of type {@code T} that represents a JsonObject.
+ * The building process can be configured to utilize a custom deserializer and a specific
+ * {@link TypeToken} which will determine the type of object built by the builder.
+ * <p>
+ * <i>Note that the representing class does not need to actually extend {@code JsonObject} class.</i>
+ *
+ * @param <T> type of object that is expected to be the result of building with this builder.
+ * In actuality the result is specified by {@link TypeToken} but it is also defined by this type.
+ */
+public class JsonObjectBuilder<T> {
+
+    private @Nullable Class<T> jsonObjectType;
+    private @Nullable Class<? extends JsonDeserializer<T>> deserializer;
+    private @Nullable TypeToken<?> typeToken;
+
+    @Contract("_ -> this")
+    public JsonObjectBuilder<T> ofType(Class<T> type) {
+        jsonObjectType = type;
+        return this;
+    }
+
+    @Contract("_ -> this")
+    public JsonObjectBuilder<T> withTypeToken(TypeToken<List<T>> token) {
+        typeToken = token;
+        return this;
+    }
+
+    @Contract("_ -> this")
+    public JsonObjectBuilder<T> withDeserializer(Class<? extends JsonDeserializer<T>> clazz) {
+        deserializer = clazz;
+        return this;
+    }
+
+    @Contract("-> new")
+    public static <T> JsonObjectBuilder<T> create() {
+        return new JsonObjectBuilder<>();
+    }
+
+    /**
+     * Build JSON object by deserializing the JSON file under given path.
+     *
+     * @param path path to JSON file as resource.
+     * @return JSON object as result of deserializing a JSON file.
+     *
+     * @throws IOException if an exception occurred while creating new {@code BufferedReader}.
+     * @throws FileNotFoundException when the resource under given path was not found.
+     * @throws IllegalStateException if needed builder members are not initialized.
+     */
+    public List<T> build(Path path) throws IOException, IllegalStateException {
+
+        if (jsonObjectType == null) {
+            throw new IllegalStateException("jsonObjectType was not defined");
+        }
+        if (typeToken == null) {
+            throw new IllegalStateException("typeToken was not defined");
+        }
+        String sPath = '/' + path.toString();
+        InputStream stream = jsonObjectType.getResourceAsStream(sPath);
+        if (stream == null) {
+            throw new FileNotFoundException("Unable getResourceAsStream from path: " + sPath);
+        }
+        try (Reader reader = new BufferedReader(new InputStreamReader(stream))) {
+            GsonBuilder builder = new GsonBuilder();
+            if (deserializer != null) {
+                builder.registerTypeAdapter(jsonObjectType, deserializer.getConstructor().newInstance());
+            }
+            // convert JSON array to list of users
+            return builder.create().fromJson(reader, typeToken.getType());
+        }
+        catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Build JSON object by deserializing the JSON from given string.
+     *
+     * @param jsonContent string to deserialize.
+     * @return JSON object as result of deserializing the string.
+     *
+     * @throws IllegalStateException if needed builder members are not initialized.
+     */
+    public List<T> build(String jsonContent) throws IllegalStateException {
+
+        GsonBuilder builder = new GsonBuilder();
+        if (deserializer != null) {
+            if (jsonObjectType == null) {
+                throw new IllegalStateException("jsonObjectType was not defined");
+            }
+            try {
+                // get default non-parameter class constructor
+                // when not declared the default constructor should be inaccessible
+                Constructor<?> constructor = deserializer.getDeclaredConstructors()[0];
+                constructor.setAccessible(true);
+
+                builder.registerTypeAdapter(jsonObjectType, constructor.newInstance());
+            }
+            catch (ReflectiveOperationException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if (typeToken == null) {
+            throw new IllegalStateException("typeToken was not defined");
+        }
+        // convert JSON array to list of users
+        return builder.create().fromJson(jsonContent, typeToken.getType());
+    }
+}
---
+++ src/test/java/io/matshou/cata/tilecov/json/JsonObjectBuilderTest.java
@@ -0,0 +1,124 @@
+package io.matshou.cata.tilecov.json;
+
+import java.io.FileNotFoundException;
+import java.lang.reflect.Type;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+public class JsonObjectBuilderTest {
+
+    private static class TestJsonObject {
+
+        // this property is expected to be initialized by Gson
+        private String jsonProperty;
+
+        // this property is expected NOT to be initialized by Gson
+        private String pseudoProperty;
+    }
+
+    private static class TestJsonObjectDeserializer implements JsonDeserializer<TestJsonObject> {
+
+        @Override
+        public TestJsonObject deserialize(JsonElement json, Type type,
+                                          JsonDeserializationContext context) throws JsonParseException {
+
+            TestJsonObject jsonObject = new Gson().fromJson(json, TestJsonObject.class);
+
+            jsonObject.jsonProperty = "2";
+            jsonObject.pseudoProperty = "value";
+
+            return jsonObject;
+        }
+    }
+
+    @Test
+    void shouldThrowExceptionWhenResourceFileNotFound() {
+
+        Executable constructWithPseudoPath = () ->
+                JsonObjectBuilder.<TestJsonObject>create()
+                        .ofType(TestJsonObject.class)
+                        .withTypeToken(new TypeToken<>() {})
+                        .build(Paths.get("pseudo.json"));
+
+        Assertions.assertThrows(FileNotFoundException.class, constructWithPseudoPath);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenMembersNotInitialized() {
+
+        // type was not initialized
+        Executable constructWithoutType = () ->
+                JsonObjectBuilder.<TestJsonObject>create()
+                        .withTypeToken(new TypeToken<>() {})
+                        .build(Paths.get("pseudo.json"));
+
+        Assertions.assertThrows(IllegalStateException.class, constructWithoutType);
+
+        constructWithoutType = () ->
+                JsonObjectBuilder.<TestJsonObject>create()
+                        .withDeserializer(TestJsonObjectDeserializer.class)
+                        .withTypeToken(new TypeToken<>() {})
+                        .build("");
+
+        Assertions.assertThrows(IllegalStateException.class, constructWithoutType);
+
+        // token was not initialized
+        Executable constructWithoutToken = () ->
+                JsonObjectBuilder.<TestJsonObject>create()
+                        .ofType(TestJsonObject.class)
+                        .build(Paths.get("pseudo.json"));
+
+        Assertions.assertThrows(IllegalStateException.class, constructWithoutToken);
+
+        constructWithoutToken = () ->
+                JsonObjectBuilder.<TestJsonObject>create()
+                        .ofType(TestJsonObject.class)
+                        .build("");
+
+        Assertions.assertThrows(IllegalStateException.class, constructWithoutToken);
+    }
+
+    @Test
+    void shouldDeserializeSinglePropertyToClassObject() {
+
+        List<String> jsonInList = List.of(
+                "[",
+                "  {",
+                "    \"jsonProperty\": 1",
+                "  }",
+                "]"
+        );
+        String jsonString = String.join("", jsonInList);
+        List<TestJsonObject> jsonObjects = JsonObjectBuilder.<TestJsonObject>create()
+                .ofType(TestJsonObject.class)
+                .withTypeToken(new TypeToken<>() {})
+                .build(jsonString);
+
+        TestJsonObject jsonObject = jsonObjects.get(0);
+        Assertions.assertEquals(1, jsonObjects.size());
+        Assertions.assertEquals("1", jsonObject.jsonProperty);
+        Assertions.assertNull(jsonObject.pseudoProperty);
+    }
+
+    @Test
+    void shouldSupportCustomDeserializer() {
+
+        String jsonString = String.join("", List.of("[", "{", "}", "]"));
+        List<TestJsonObject> jsonObjects = JsonObjectBuilder.<TestJsonObject>create()
+                .ofType(TestJsonObject.class)
+                .withTypeToken(new TypeToken<>() {})
+                .withDeserializer(TestJsonObjectDeserializer.class)
+                .build(jsonString);
+
+        TestJsonObject jsonObject = jsonObjects.get(0);
+        Assertions.assertEquals("2", jsonObject.jsonProperty);
+        Assertions.assertEquals("value", jsonObject.pseudoProperty);
+    }
+}
