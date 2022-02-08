7e333b3933369ba8541130a0fefe1131a4e9aa33
Matthew Cain <matthew.cain@tuta.io>
2022-02-08 15:02:25 +0100

    Start deserializing tile atlases

--- src/main/java/io/matshou/cata/tilecov/json/TileConfigJsonDeserializer.java
+++ src/main/java/io/matshou/cata/tilecov/json/TileConfigJsonDeserializer.java
@@ -18,23 +18,50 @@ public class TileConfigJsonDeserializer extends JsonObjectDeserializer<TileConfi
         super(TileConfigJsonObject.class);
     }
 
+    private void changeFieldValue(Field field, TileConfigJsonObject target, Object value) {
+
+        try {
+            field.set(target, value);
+        }
+        catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void deserializeTileInfo(TileConfigJsonObject target, JsonElement element) {
+
+        List<TileInfoJsonObject> jsonObjects = JsonObjectBuilder.<TileInfoJsonObject>create()
+                .ofType(TileInfoJsonObject.class)
+                .withListTypeToken(new TypeToken<>() {})
+                .buildAsList(element.toString());
+
+        Field field = Objects.requireNonNull(jsonObjectFields.get("tile_info"));
+        changeFieldValue(field, target, jsonObjects.get(0));
+    }
+
+    private void deserializeTileAtlases(TileConfigJsonObject target, JsonElement element) {
+
+        List<TileAtlasJsonObject> jsonObjects = JsonObjectBuilder.<TileAtlasJsonObject>create()
+                .ofType(TileAtlasJsonObject.class)
+                .withListTypeToken(new TypeToken<>() {})
+                .buildAsList(element.toString());
+
+        Field field = Objects.requireNonNull(jsonObjectFields.get("tiles-new"));
+        changeFieldValue(field, target, jsonObjects);
+    }
+
     @Override
     void deserializeObjectMembers(Gson gson, JsonObject jsonObject, TileConfigJsonObject target, String entry) {
 
         JsonElement element = jsonObject.get(entry);
-        if (element != null) {
-            List<TileInfoJsonObject> jsonObjects =
-                    JsonObjectBuilder.<TileInfoJsonObject>create()
-                            .ofType(TileInfoJsonObject.class)
-                            .withListTypeToken(new TypeToken<>() {})
-                            .buildAsList(element.toString());
-            try {
-                Field tileInfoField = Objects.requireNonNull(jsonObjectFields.get("tile_info"));
-                tileInfoField.set(target, jsonObjects.get(0));
-            }
-            catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
+        if (element == null) {
+            return;
+        }
+        if (entry.equals("tile_info")) {
+            deserializeTileInfo(target, element);
+        }
+        else if (entry.equals("tiles-new")) {
+            deserializeTileAtlases(target, element);
         }
     }
 }
--- src/test/java/io/matshou/cata/tilecov/json/TileConfigJsonObjectTest.java
+++ src/test/java/io/matshou/cata/tilecov/json/TileConfigJsonObjectTest.java
@@ -2,24 +2,31 @@ package io.matshou.cata.tilecov.json;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.google.gson.reflect.TypeToken;
 
 public class TileConfigJsonObjectTest {
 
-    @Test
-    void shouldDeserializeTileInfoFromJson() throws IOException {
+    private TileConfigJsonObject tileConfig;
 
-        TileConfigJsonObject tileConfig =
-                JsonObjectBuilder.<TileConfigJsonObject>create()
-                        .ofType(TileConfigJsonObject.class)
-                        .withTypeToken(new TypeToken<>() {})
-                        .withDeserializer(TileConfigJsonDeserializer.class)
-                        .build(Paths.get("tile_config.json"));
+    @BeforeEach
+    void readTileConfigFromJson() throws IOException {
+
+        tileConfig = JsonObjectBuilder.<TileConfigJsonObject>create()
+                .ofType(TileConfigJsonObject.class)
+                .withTypeToken(new TypeToken<>() {})
+                .withDeserializer(TileConfigJsonDeserializer.class)
+                .build(Paths.get("tile_config.json"));
+    }
+
+    @Test
+    void shouldDeserializeTileInfoFromJson() {
 
         Optional<TileInfoJsonObject> oTileInfo = tileConfig.getTileInfo();
         Assertions.assertTrue(oTileInfo.isPresent());
@@ -28,5 +35,18 @@ public class TileConfigJsonObjectTest {
         Assertions.assertEquals(2, tileInfo.getPixelScale());
         Assertions.assertEquals(32, tileInfo.getWidth());
         Assertions.assertEquals(32, tileInfo.getHeight());
+    }
+
+    @Test
+    void shouldDeserializeTileAtlasesFromJson() {
+
+        List<TileAtlasJsonObject> tileAtlases = tileConfig.getTileAtlases();
+        Assertions.assertEquals(2, tileAtlases.size());
+
+        TileAtlasJsonObject tiles = tileAtlases.get(0);
+        TileAtlasJsonObject moreTiles = tileAtlases.get(1);
+
+        Assertions.assertEquals("tiles.png", tiles.getFilename());
+        Assertions.assertEquals("moretiles.png", moreTiles.getFilename());
     }
 }
