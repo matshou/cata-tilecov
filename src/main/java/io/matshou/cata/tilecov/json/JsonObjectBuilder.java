0d16735395c51ad6cf5ce30e0993e68ce9e0ce68
Matthew Cain <matthew.cain@tuta.io>
2022-02-08 14:37:31 +0100

    Expand JsonObjectBuilder class

--- src/main/java/io/matshou/cata/tilecov/json/JsonObjectBuilder.java
+++ src/main/java/io/matshou/cata/tilecov/json/JsonObjectBuilder.java
@@ -4,13 +4,14 @@ import java.io.*;
 import java.lang.reflect.Constructor;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
 
-import com.google.gson.JsonSyntaxException;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
 /**
@@ -23,6 +24,7 @@ import com.google.gson.reflect.TypeToken;
  * @param <T> type of object that is expected to be the result of building with this builder.
  * In actuality the result is specified by {@link TypeToken} but it is also defined by this type.
  */
+@SuppressWarnings("ConstantConditions")
 public class JsonObjectBuilder<T> {
 
     private @Nullable Class<T> jsonObjectType;
@@ -88,6 +90,58 @@ public class JsonObjectBuilder<T> {
         return new JsonObjectBuilder<>();
     }
 
+    @Contract("_-> new")
+    private InputStreamReader readJsonFromPath(Path path) throws IOException {
+
+        String sPath = '/' + path.toString();
+        InputStream stream = Objects.requireNonNull(jsonObjectType).getResourceAsStream(sPath);
+        if (stream == null) {
+            throw new FileNotFoundException("Unable getResourceAsStream from path: " + sPath);
+        }
+        return new InputStreamReader(stream);
+    }
+
+    @Contract("-> new")
+    private GsonBuilder startBuildFromPath() {
+
+        if (jsonObjectType == null) {
+            throw new IllegalStateException("jsonObjectType was not defined");
+        }
+        if (typeToken == null) {
+            throw new IllegalStateException("typeToken was not defined");
+        }
+        GsonBuilder builder = new GsonBuilder();
+        try {
+            if (deserializer != null) {
+                // assume there is a single constructor with no arguments
+                Constructor<?> constructor = deserializer.getDeclaredConstructor();
+                builder.registerTypeAdapter(jsonObjectType, constructor.newInstance());
+            }
+        }
+        catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+        return builder;
+    }
+
+    /**
+     * Build JSON object by deserializing the JSON file under given path.
+     *
+     * @param path path to JSON file as resource.
+     * @return list of JSON objects as result of deserializing a JSON file.
+     *
+     * @throws IOException if an exception occurred while creating new {@code BufferedReader}.
+     * @throws FileNotFoundException when the resource under given path was not found.
+     * @throws IllegalStateException if needed builder members are not initialized.
+     */
+    public List<T> buildAsList(Path path) throws IOException, IllegalStateException {
+
+        GsonBuilder builder = startBuildFromPath();
+        try (Reader reader = readJsonFromPath(path)) {
+            return builder.create().fromJson(reader, typeToken.getType());
+        }
+    }
+
     /**
      * Build JSON object by deserializing the JSON file under given path.
      *
@@ -98,47 +152,16 @@ public class JsonObjectBuilder<T> {
      * @throws FileNotFoundException when the resource under given path was not found.
      * @throws IllegalStateException if needed builder members are not initialized.
      */
-    public List<T> buildAsList(Path path) throws IOException, IllegalStateException {
+    public T build(Path path) throws IOException, IllegalStateException {
 
-        if (jsonObjectType == null) {
-            throw new IllegalStateException("jsonObjectType was not defined");
-        }
-        if (typeToken == null) {
-            throw new IllegalStateException("typeToken was not defined");
-        }
-        String sPath = '/' + path.toString();
-        InputStream stream = jsonObjectType.getResourceAsStream(sPath);
-        if (stream == null) {
-            throw new FileNotFoundException("Unable getResourceAsStream from path: " + sPath);
-        }
-        try (Reader reader = new BufferedReader(new InputStreamReader(stream))) {
-            GsonBuilder builder = new GsonBuilder();
-            if (deserializer != null) {
-                // assume there is a single constructor with no arguments
-                Constructor<?> constructor = deserializer.getDeclaredConstructor();
-                builder.registerTypeAdapter(jsonObjectType, constructor.newInstance());
-            }
-            // convert JSON array to list of users
+        GsonBuilder builder = startBuildFromPath();
+        try (Reader reader = readJsonFromPath(path)) {
             return builder.create().fromJson(reader, typeToken.getType());
-        }
-        catch (ReflectiveOperationException e) {
-            throw new RuntimeException(e);
         }
     }
 
-    /**
-     * Build list of JSON objects by deserializing the JSON from given string.
-     * <p>
-     * Note that when using this method to build you need to make sure that {@code TypeToken}
-     * set for this builder is of type {@link List} otherwise an exception will be thrown.
-     *
-     * @param jsonContent string to deserialize.
-     * @return JSON object as result of deserializing the string.
-     *
-     * @throws IllegalStateException if needed builder members are not initialized.
-     * @throws JsonSyntaxException when there was an error while building the object.
-     */
-    public List<T> buildAsList(String jsonContent) throws IllegalStateException {
+    @Contract("-> new")
+    private GsonBuilder startBuildFromString() {
 
         GsonBuilder builder = new GsonBuilder();
         if (deserializer != null) {
@@ -157,6 +180,24 @@ public class JsonObjectBuilder<T> {
         if (typeToken == null) {
             throw new IllegalStateException("typeToken was not defined");
         }
+        return builder;
+    }
+
+    /**
+     * Build list of JSON objects by deserializing the JSON from given string.
+     * <p>
+     * Note that when using this method to build you need to make sure that {@code TypeToken}
+     * set for this builder is of type {@link List} otherwise an exception will be thrown.
+     *
+     * @param jsonContent string to deserialize.
+     * @return list of JSON objects as result of deserializing the string.
+     *
+     * @throws IllegalStateException if needed builder members are not initialized.
+     * @throws JsonSyntaxException when there was an error while building the object.
+     */
+    public List<T> buildAsList(String jsonContent) throws IllegalStateException {
+
+        GsonBuilder builder = startBuildFromString();
         return builder.create().fromJson(jsonContent, typeToken.getType());
     }
 
@@ -174,23 +215,7 @@ public class JsonObjectBuilder<T> {
      */
     public T build(String jsonContent) throws IllegalStateException {
 
-        GsonBuilder builder = new GsonBuilder();
-        if (deserializer != null) {
-            if (jsonObjectType == null) {
-                throw new IllegalStateException("jsonObjectType was not defined");
-            }
-            try {
-                // assume there is a single constructor with no arguments
-                Constructor<?> constructor = deserializer.getDeclaredConstructor();
-                builder.registerTypeAdapter(jsonObjectType, constructor.newInstance());
-            }
-            catch (ReflectiveOperationException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        if (typeToken == null) {
-            throw new IllegalStateException("typeToken was not defined");
-        }
+        GsonBuilder builder = startBuildFromString();
         return builder.create().fromJson(jsonContent, typeToken.getType());
     }
 }
--- src/test/java/io/matshou/cata/tilecov/json/TileConfigJsonObjectTest.java
+++ src/test/java/io/matshou/cata/tilecov/json/TileConfigJsonObjectTest.java
@@ -1,34 +1,31 @@
 package io.matshou.cata.tilecov.json;
 
-import com.google.gson.reflect.TypeToken;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
-import java.util.Optional;
+import com.google.gson.reflect.TypeToken;
 
 public class TileConfigJsonObjectTest {
 
     @Test
-    void shouldDeserializeTileInfoFromJson() {
+    void shouldDeserializeTileInfoFromJson() throws IOException {
 
-        String jsonString = JsonObjectTestHelper.createJsonString("tile_info", Map.of(
-                "pixelscale", "1",
-                "width", "32",
-                "height", "32"
-        ));
         TileConfigJsonObject tileConfig =
                 JsonObjectBuilder.<TileConfigJsonObject>create()
                         .ofType(TileConfigJsonObject.class)
                         .withTypeToken(new TypeToken<>() {})
                         .withDeserializer(TileConfigJsonDeserializer.class)
-                        .build(jsonString);
+                        .build(Paths.get("tile_config.json"));
 
         Optional<TileInfoJsonObject> oTileInfo = tileConfig.getTileInfo();
         Assertions.assertTrue(oTileInfo.isPresent());
         TileInfoJsonObject tileInfo = oTileInfo.get();
 
-        Assertions.assertEquals(1, tileInfo.getPixelScale());
+        Assertions.assertEquals(2, tileInfo.getPixelScale());
         Assertions.assertEquals(32, tileInfo.getWidth());
         Assertions.assertEquals(32, tileInfo.getHeight());
     }
